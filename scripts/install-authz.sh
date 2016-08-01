8075be8fbc01c6dd45bf3921a2c75a479883678b
Everett Toews <everett.toews@rackspace.com>
2016-07-31 23:13:46 -0500

    Update to use boot2docker

--- scripts/install-authz.sh
+++ scripts/install-authz.sh
@@ -1,26 +1,14 @@
 #!/bin/bash
 
-# get the python plugin code
-apt-get -y install python-dev python-pip
-git clone https://github.com/everett-toews/docker-authz-python-plugin.git
-cd docker-authz-python-plugin
+# use the plugin when the docker daemon starts
+sed -i 's/.*provider.*/&\n--authorization-plugin=authz/' /var/lib/boot2docker/profile
+
+# initialize our "datastore"
+echo "False" > /var/run/authz-said-hello.txt
 
 # install the dependencies and run the plugin
 pip install -r requirements.txt
-mkdir /run/docker/plugins/
 uwsgi --ini uwsgi.ini &
 
-# modify the docker service start up to look for the plugin
-mkdir /etc/systemd/system/docker.service.d
-cat << EOF > /etc/systemd/system/docker.service.d/authz.conf
-[Service]
-ExecStart=
-ExecStart=/usr/bin/dockerd -H fd:// --authorization-plugin=authz
-EOF
-
-# initialize our datastore :)
-echo "False" > /var/run/authz-said-hello.txt
-
-# reload systemd and restart the docker service
-systemctl daemon-reload
-systemctl restart docker
+# restart the docker daemon
+/etc/init.d/docker restart
